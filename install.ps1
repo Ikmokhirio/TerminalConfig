c94c964dcc7e03d2a53620e57561598f8d5fa63f
Ikmokhirio <Ikmokhirio@mail.ru>
2022-08-12 22:18:52 +0300

    Fix : Folder creating

--- install.ps1
+++ install.ps1
@@ -1,5 +1,6 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
 Install-Module -Name Terminal-Icons -Repository PSGallery
+mkdir $env:HOMEPATH\Documents\PowerShell -Force
 cp .\Microsoft.PowerShell_profile.ps1 $env:HOMEPATH\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
-mkdir $env:HOMEPATH\posh_config
+mkdir $env:HOMEPATH\posh_config -Force
 cp .\atomic.omp.json $env:HOMEPATH\posh_config\atomic.omp.json
