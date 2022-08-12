da22c25892c1bb28e414cea49041d3aacf64d7ef
Ikmokhirio <Ikmokhirio@mail.ru>
2022-08-12 22:13:32 +0300

    Fix : PowerShell script path

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
 Install-Module -Name Terminal-Icons -Repository PSGallery
-cp .\Microsoft.PowerShell_profile.ps1 $env:HOMEPATH\Documents\PowerShell\Microsoft.PowerShell_profile.ps1i
+cp .\Microsoft.PowerShell_profile.ps1 $env:HOMEPATH\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 mkdir $env:HOMEPATH\posh_config
 cp .\atomic.omp.json $env:HOMEPATH\posh_config\atomic.omp.json
